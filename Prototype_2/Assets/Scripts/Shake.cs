3c52149932e9fcb7e5c97eb11cf27002dd640239
Ximena Camacho <xcamacho@usc.edu>
2026-02-02 12:24:33 -0800

    Screen Shake Code Added
    Not Enabled

---
+++ Prototype_2/Assets/Scripts/Shake.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public float duration = 1f;
+    public bool start = false;
+    public AnimationCurve curve;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (start)
+        {
+            start = false;
+            StartCoroutine(Shaking());
+        }
+
+    }
+
+    IEnumerator Shaking()
+    {
+        Vector3 startPosition = transform.position;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float strength = curve.Evaluate(elapsedTime / duration);
+            transform.position = startPosition + Random.insideUnitSphere * strength;
+            yield return null;
+        }
+
+        transform.position = startPosition;
+    }
+}
